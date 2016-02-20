2ad6f3eddab908043945108a886660d6cb51090e
cindy bishop <cindyloo_home@yahoo.com>
2016-02-20 08:36:33 -0500

    server.js updates

--- server.js
+++ server.js
@@ -6,159 +6,92 @@ filesys = require("fs");
 glob = require("glob");
 
 
+my_http.createServer(function(request, response) {
+	
+	var headers = request.headers;
+	var method = request.method;
+	
+	var body = [];
+	  
+	var my_path = url.parse(request.url).pathname;
+	var url_parts = url.parse(request.url);
+	var full_path = path.join(process.cwd(),my_path);
+	var data_req = url_parts.query;
+		
+	  request.on('error', function(err) {
+		console.error(err);
+	  }).on('data', function(chunk) {
+	  console.log("data incoming " + chunk);
+		body.push(chunk);
+	  }).on('end', function() {
+	  	  console.log("request was " + request.url );
+		body = Buffer.concat(body).toString();
+		
+		if (request.method === 'POST' && request.url === '/writeObj') {
+    		writeOBJ(".", body);
+    	}
+		
+		response.on('error', function(err) {
+		  console.error(err);
+		});
 
-my_http.createServer(function(request,response){
-    var my_path = url.parse(request.url).pathname;
-    var url_parts = url.parse(request.url);
-    var full_path = path.join(process.cwd(),my_path);
-    
-   
-    if(my_path=="/getconfig"){
-       sys.puts("request recieved");
-        var string = readJSONConfig("/assets/"); //TODO get path param
-        sys.puts("string '" + JSON.stringify(string) + "' chosen");
-		response.writeHeader(200,{'Content-Type':'application/json', 'Content-Length':JSON.stringify(string).length});
-		//response.setHeader("Cache-control", "");
-		//response.setHeader("Pragma", "");
-		response.write(JSON.stringify(string));
-  		
-        response.end();
-        sys.puts("string sent");
-    }
-    else if(my_path=="/getPics"){
-    	sys.puts("getpics received");
-        var files = getImageFiles("/assets/haunted/",function(files){
-					 sys.puts("string '" + JSON.stringify(files) + "' chosen");
-							response.writeHeader(200,{'Content-Type':'application/json', 'Content-Length':JSON.stringify(files).length});
-							response.write(JSON.stringify(files));
-							response.end();
-							sys.puts("string sent");
-					}); //TODO get path param
-        
-    
-    
-    }else if(my_path=="/getTwitchIFrame"){
-    	sys.puts("client iframe received");
-      //'http://twitch.tv/cindyloo477/chat?popout=' });
-		//needs www to work.  also still errors with api cross-domain stuff
-		//what if I saved this as a file, then pointed to it from local server?
-		//still kinda confused how to handle all dependencies
-		//document.domain stuff?
-			var destParams = url.parse('http://www.twitch.tv/cindyloo477/chat/');
-
-			var reqOptions = {
-				host : destParams.host,
-				port : 80,
-				path : destParams.pathname,
-				method : "GET"
-			};
-		
-			var req = my_http.request(reqOptions, function(res) {
-				var headers = res.headers;
-				headers['Access-Control-Allow-Origin'] = '*';
-				headers['Access-Control-Allow-Headers'] = '*';
-		
-				response.writeHead(200, headers);
-		
-				res.on('data', function(chunk) {
-					response.write(chunk);
-				});
-		
-				res.on('end', function() {
-					response.end();
-				});
-			});
-		
-			req.on('error', function(e) {
-				console.log('An error occured: ' + e.message);
-				response.writeHead(503);
-				response.write("Error!");
+ 		 path.exists(full_path,function(exists){
+		sys.puts("full path" + full_path);
+		if(!exists){
+				response.writeHeader(404, {"Content-Type": "text/plain"});  
+				response.write("node sez 404 Not Found\n");  
 				response.end();
-			});
-			req.end();
+		}
+		else{
 			
-    }
-    else {
-    path.exists(full_path,function(exists){
-    sys.puts("full path" + full_path);
-    if(!exists){
-            response.writeHeader(404, {"Content-Type": "text/plain"});  
-            response.write("node sez 404 Not Found\n");  
-            response.end();
-        }
-        else{
-        
-            filesys.readFile(full_path, "binary", function(err, file) {  
-                 if(err) {  
-                     response.writeHeader(500, {"Content-Type": "text/plain"});  
-                     response.write(err + "\n");  
-                     response.end();  
-                
-                 }  
-                 else{
-                    response.writeHeader(200);  
-                    response.write(file, "binary");  
-                    response.end();
-                }
-                      
-            });
-        }
+				
+			
+				filesys.readFile(full_path, "binary", function(err, file) {  
+					 if(err) {  
+						 response.writeHeader(500, {"Content-Type": "text/plain"});  
+						 response.write(err + "\n");  
+						 response.end();  
+					
+					 }  
+					 else{
+						response.writeHeader(200);  
+						response.write(file, "binary");  
+						response.end();
+					}
+						  
+				});
+		}
     });
-    }
-    
-    
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+
+    // END OF NEW STUFF
+  });
+  
+  if (request.method === 'POST' && request.url === '/writeObj') {
+    writeOBJ(".", body);
+  } 
+  
 }).listen(8080);
 sys.puts("yay, Server Running on 8080");  
 
 
 
-
-function getImageFiles(path, sendBack){
-	// options is optional
-	return glob("assets/haunted/*.jpg", function (er, files) {
-  	
-  		sys.puts("files are " + files);
-  		sendBack(files);
-  		})
-}
-
-function readJSONConfig(path){
-				var data = filesys.readFileSync(process.cwd() + "/" + path + 'config.json'),
-				 myObj;
-		
-			  try {
-				myObj = JSON.parse(data);
-				console.dir(myObj);
-			  }
-			  catch (err) {
-				console.log('There has been an error parsing your JSON.')
-				console.log(err);
-			  }
-			  
-			  return myObj;
-}
-		
-function writeJSONConfig(path, data){
+function writeOBJ(path, data){
 		  if (data == null)
-		  	data = {
-				name: 'Person',
-				object: 'box',
-				width: '20',
-				height: '10',
-				depth: '20',
-				addressX: '100',
-				addressY: '100',
-				addressZ: '100'
-		  };
+		  	console.log('There has been an error getting your data.');
+	
+		//probably shoudl do some kind of checking here...
 		
-		  var data = JSON.stringify(myOptions);
-		
-		  filesys.writeFile(path + 'config.json', data, function (err) {
+		console.log('data ' + data);
+		  filesys.writeFile('test.obj', data, function (err) {
 			if (err) {
-			  console.log('There has been an error saving your configuration data.');
+			  console.log('There has been an error saving your obj.');
 			  console.log(err.message);
 			  return;
 			}
-			console.log('Configuration saved successfully.')
+			console.log('OBJ saved successfully.')
 		  });
 		}		
+		
+		
